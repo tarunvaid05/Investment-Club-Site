c680f64e733c368a777b913abeed5996eaf9b998
tarunvaid05 <tarunvaid05@gmail.com>
2024-12-30 02:03:58 -0500

    changed file structure back

--- docs/app.py
+++ docs/app.py
@@ -1,6 +1,12 @@
 from flask import Flask, render_template
 import os
-app = Flask(__name__)
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+
+app = Flask(__name__, 
+           template_folder=current_dir,  
+           static_folder=current_dir,    
+           static_url_path='')         
 
 @app.route('/')
 def home():
